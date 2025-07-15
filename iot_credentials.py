4bfa45abd496570139a67a08118582c9b4f45f89
vjx <excalibas@gmail.com>
2025-07-15 11:28:13 +0100

    Add files via upload

---
+++ iot_credentials.py
@@ -0,0 +1,8 @@
+THINGSPEAK_WRITE_API_KEY = 'HJKMGQ3MKHGFCREAC' #Change to your API KEY
+
+# Lista de redes Wi-Fi conhecidas (SSID, senha)
+wifi_networks = [
+    ("NAME", "PASS"),
+    ("OTHERNAME", "OTHERPASS"),
+
+]
